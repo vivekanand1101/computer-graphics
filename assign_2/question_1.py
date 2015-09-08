52413320ef9d0e68369f8e3d63f4a64f2e5df7f1
Vivek Anand <vivekanand1101@gmail.com>
2015-09-08 22:27:04 +0530

    Assign 2 - 1, 2, 3 added

---
+++ assign_2/question_1.py
@@ -0,0 +1,5 @@
+from math import sqrt 
+print "%d,%d,%d" % (0 - (80), 0 - (80), 0)
+print "%d,%d,%d" % (80, 0 - (80), 0)
+print "%d,%d,%d" % (80, 80,  0)
+print "%d,%d,%d" % (0 - 80, 80, 0)
---
+++ assign_2/question_2.py
@@ -0,0 +1,4 @@
+from math import sqrt
+for i in range(0, 9):
+        for j in range(0, 9):
+                print "%d,%d,%d" % ((-80 + j * 160/8), (-80 + i * 160/8), 0)
